d675bd030fb3aba0ac51e49137f99f71fe78e928
MarcAntoineLebourgeois <marc.antoine.lebourgeois@gmail.com>
2020-09-18 09:53:53 +0800

    delete useless Maintheme and darktheme

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -18,33 +18,6 @@ import listeReplyPhilosophe from "./Data/ListePhilosophes"
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 //import { blue,yellow,red } from '@material-ui/core/colors';
 
-const Maintheme = {
-    palette: {
-        primary: {
-            main: "#ea8f8f",
-          },
-        secondary: {
-            main: '#52CDD1',
-          },
-        type: 'light'
-    }
-}
-
-const useDarkMode = () => {
-    const [theme,setTheme] = useState(Maintheme)
-    const { palette : {type}} = theme;
-    const DarkModeOn = () => {
-        const updatedTheme = {
-            ...theme,
-            palette:{
-                ...theme.palette,
-                type: type === 'light' ? 'dark' : 'light'
-            }
-        }
-        setTheme(updatedTheme)
-    }
-    return[ theme, DarkModeOn ]
-}
 
 const App = () => {
 	
