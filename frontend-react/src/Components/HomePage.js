0273775577c17dc14cb18dc4db666d590d8df6cb
MarcAntoineLebourgeois <marc.antoine.lebourgeois@gmail.com>
2020-09-18 13:30:51 +0800

    correction of homepage props

--- frontend-react/src/Components/HomePage.js
+++ frontend-react/src/Components/HomePage.js
@@ -7,55 +7,18 @@ import SelectionPanel from "./SelectionPanel"
 import ThreeBlocks from "./ThreeBlocks"
 import BottomBar from "./BottomBar"
 
-const HomePage = ({
-	DarkModeOn,
-	FuncShowRatingForm,
-	showBienvenue,
-	reinitForm,handleSubmit,
-    ChangeResponseDicts, ChangeMots,
-    form, setForm,
-    setDissert, dissert,
-    listeReplyTheme,ExempleThemes,
-    listeReplyPhilosophe,ExemplePhilosophe,
-	setShowRendu,setRoute
-}) => {
+const HomePage = (props) => {
 
 	return(
 
 	<>
-      <Grid className="BackgroundPage" style={{padding:10, height: '60vh'}}>
-          
-		  <AppBarFront 
-            DarkModeOn={DarkModeOn}
-            FuncShowRatingForm={FuncShowRatingForm}
-          />
-		  
-          <SelectionPanel
-            showBienvenue={showBienvenue}
-            reinitForm={reinitForm} 
-            ChangeResponseDicts={ChangeResponseDicts} 
-            handleSubmit = {handleSubmit}
-            form = {form}
-            setForm={setForm}
-            ChangeMots={ChangeMots}
-            setDissert={setDissert}
-            dissert={dissert}
-            ExempleThemes={ExempleThemes}
-            ExemplePhilosophe={ExemplePhilosophe}
-            setShowRendu={setShowRendu}
-            listeReplyTheme={listeReplyTheme}
-            listeReplyPhilosophe={listeReplyPhilosophe}
-			setRoute = {setRoute}
-            />
-			
-      </Grid>
-
-	 <ThreeBlocks             
-		reinitForm={reinitForm} 
-	  />
-
+    <Grid className="BackgroundPage" style={{padding:10, height: '60vh'}}>
+      <AppBarFront {...props} />
+      <SelectionPanel {...props}  />
+    </Grid>
+	  <ThreeBlocks {...props} />
 	  <BottomBar/>
-		</>  
+	</>  
 		  )
 }
 
