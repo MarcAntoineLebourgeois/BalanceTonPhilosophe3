a57c4cae110f9aeb1acde923156cdbd9a4f7e920
unknown <marc.antoine.lebourgeois@gmail.com>
2020-09-25 11:12:40 +0800

    not finish home page blocks css

--- frontend-react/src/Components/ThreeBlocks.js
+++ frontend-react/src/Components/ThreeBlocks.js
@@ -3,8 +3,10 @@ import {Grid,Typography, Card,CardActionArea,CardMedia,CardContent} from "@mater
 import Background1 from "../Data/dissertImage.jpg"
 import Background2 from "../Data/themesImage.jpg"
 import Background3 from "../Data/philosopherImage.jpg"
+import Background4 from "../Data/quizImage.jpg"
 import { makeStyles } from '@material-ui/core/styles';
 import { Link} from "react-router-dom";
+import "../styles.css";
 
 const ThreeBlocks = (props) => {
     const useStyles = makeStyles(theme => ({
@@ -14,77 +16,103 @@ const ThreeBlocks = (props) => {
         }
       }));
     const classes = useStyles();
-return(
-    <div>
+    return(
+    <div > 
         <Grid style={{height:"70px",margin:"auto"}} container direction='row' alignItems="flex-end" justify="space-around">
             <Typography> Utiliser BalanceTonPhilosophe </Typography>
         </Grid>
+        <p></p>
         <Grid 
             container 
+            item
             direction='row'
             alignItems="center" 
             justify="space-around" 
-            xs={12} sm={12} md={10} lg={8} xl={8} 
+            xs={12} sm={12} md={10} lg={10} xl={10} 
             style={{margin: "auto"}}
         >
         
         <Link to='/dissertations' style={{ textDecoration: 'none' }}>
-            <Grid container item style={{width: '300px', height: '70vh' }}>
-                <Grid item style={{height:"90%", margin: "auto"}}>
-                    <Card onClick={props.reinitForm} >
+            <Grid container item style={{width: '280px' }}>
+
+                <Card onClick={props.reinitForm} >
                         <CardActionArea>
                             <CardMedia className={classes.media} image={Background1} />
                             <CardContent style={{backgroundColor: "#ea8f8f" }}>
                             <Typography gutterBottom variant="h5" component="h2"> Pour les dissertations </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" align='justify'>
+                            <Typography variant="body2" color="textSecondary" align='justify'>
                                 Si tu cherches des sources a un sujet de dissertation, notre IA peut t'aider. 
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" align='justify'>
                                 Elle cherchera les themes qui ressortent en analysant les mots puis te proposera des philosophes.
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" align='justify'>    
                                 Attention toutefois, elle peut parfois etre a cote de la plaque :)
                             </Typography>
                             </CardContent>
                         </CardActionArea>
                     </Card>
-                </Grid>
+
             </Grid>
         </Link>
         <Link to='/themes' style={{ textDecoration: 'none' }}>
-            <Grid container item style={{width: '300px', height: '70vh' }}>
-                <Grid item style={{height:"90%", margin: "auto"}}>
-                    <Card onClick={props.reinitForm} >
+            <Grid container item style={{width: '280px'}}>
+                <Card onClick={props.reinitForm} >
                         <CardActionArea>
                             <CardMedia className={classes.media} image={Background2} />
                             <CardContent style={{backgroundColor: "#ea8f8f" }}>
                             <Typography gutterBottom variant="h5" component="h2"> Pour les themes </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" align='justify'>
-                                Ici, tu peux chercher quel philosophe a ecrit sur un theme. 
+                            <Typography variant="body2" color="textSecondary" align='justify'>
+                                Ici, tu peux chercher quel philosophe a ecrit sur un theme.
+                            </Typography> 
+                            <Typography variant="body2" color="textSecondary" align='justify'>
                                 L'avantage, c'est que tu peux mettre plusieurs themes.
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" align='justify'>
                                 Cela permettra d'etre beaucoup plus precis dans le rendu et trouver la meilleure source :)
                             </Typography>
                             </CardContent>
                         </CardActionArea>
                     </Card>                
-                </Grid>
             </Grid>
         </Link>
         <Link to='/philosophes' style={{ textDecoration: 'none' }}>
-            <Grid container item style={{width: '300px', height: '70vh' }}>
-                <Grid item style={{height:"90%", margin: "auto"}}>
-                    <Card onClick={props.reinitForm} >
-                        <CardActionArea>
-                            <CardMedia className={classes.media} image={Background3} />
-                            <CardContent style={{backgroundColor: "#ea8f8f" }}>
-                            <Typography gutterBottom variant="h5" component="h2"> Pour les philosophes </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" align='justify'>
-                                Difficile de trouver un resume convenable de la pensee d'un philosophe sur les themes du bac, non?
-                                Via cette recherche, tu pourras facilement le trouver. C'est aussi plutot pratique pour comparer deux philosophes. 
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>  
-                </Grid>
+            <Grid container item style={{width: '280px' }}>
+                <Card onClick={props.reinitForm} >
+                    <CardActionArea>
+                        <CardMedia className={classes.media} image={Background3} />
+                        <CardContent style={{backgroundColor: "#ea8f8f" }}>
+                        <Typography gutterBottom variant="h5" component="h2"> Pour les philosophes </Typography>
+                        <Typography variant="body2" color="textSecondary" align='justify'>
+                            Difficile de trouver un resume convenable de la pensee d'un philosophe sur les themes du bac?
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" align='justify'>
+                            Via cette recherche, tu pourras facilement le trouver. C'est aussi plutot pratique pour comparer deux philosophes. 
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>  
             </Grid>
         </Link>
-
+        <Link to='/quiz' style={{ textDecoration: 'none' }}>
+            <Grid container item style={{width: '280px' }}>
+                <Card onClick={props.reinitForm} >
+                    <CardActionArea>
+                        <CardMedia className={classes.media} image={Background4} />
+                        <CardContent style={{backgroundColor: "#ea8f8f" }}>
+                        <Typography gutterBottom variant="h5" component="h2"> Les Quiz </Typography>
+                        <Typography variant="body2" color="textSecondary" align='justify'>
+                            Teste tes connaissances avec ces quiz qui t'entraineront a te rappeler des diverses punchlines. 
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" align='justify'>
+                            Aux professeurs: cela peut aussi etre un bon moyen pour evaluer les eleves via un quiz.
+                            Cliquez pour plus de details. 
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>  
+            </Grid>
+        </Link>
         </Grid>
 
         <p></p>
