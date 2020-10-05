73432bcde70b04444b3487bbd65d8b6f67bea59f
EC2 Default User <ec2-user@ip-172-31-90-1.ec2.internal>
2020-10-05 09:08:56 +0000

    add cognito info in config,js and index.js

---
+++ frontend-react/src/config.js
@@ -0,0 +1,18 @@
+
+export default {
+	  s3: {
+		      REGION: "ap-southeast-1",
+		      BUCKET: "www.balancetonphilosophe.com"
+		    },
+	  apiGateway: {
+		      REGION: "YOUR_API_GATEWAY_REGION",
+		      URL: "YOUR_API_GATEWAY_URL"
+		    },
+	  cognito: {
+		      REGION: "us-east-1",
+		      USER_POOL_ID: "us-east-1_p1KM6gMPB",
+		      APP_CLIENT_ID: "6b2m4kib32smj0g194gp7hh347",
+		      IDENTITY_POOL_ID: "us-east-1:9285ae78-e96c-4f47-bf70-4ecea67f8660" 
+		    }
+};
+
--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -3,7 +3,8 @@ import React from "react";
 //import ReactDOM from "react-dom";
 import { render } from 'react-snapshot';
 import App from "./App";
-
+import {Amplify} from 'aws-amplify';
+import config from './config';
 
 //const rootElement = document.getElementById("root");
 
@@ -13,6 +14,30 @@ ReactDOM.render(
     , rootElement
 );
 */
+
+Amplify.configure({
+	  Auth: {
+		      mandatorySignIn: true,
+		      region: config.cognito.REGION,
+		      userPoolId: config.cognito.USER_POOL_ID,
+		      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+		      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+		    },
+	  Storage: {
+		      region: config.s3.REGION,
+		      bucket: config.s3.BUCKET,
+		      identityPoolId: config.cognito.IDENTITY_POOL_ID
+		    },
+	  API: {
+		      endpoints: [
+			            {
+					            name: "notes",
+					            endpoint: config.apiGateway.URL,
+					            region: config.apiGateway.REGION
+					          },
+			          ]
+		    }
+});
 
 render(
     <App/>,
